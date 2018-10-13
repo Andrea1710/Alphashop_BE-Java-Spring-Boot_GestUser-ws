8727c3d8b1f202b2e0812cd6d9911b821acb75e4
Nicola La Rocca <nicola@kalsyfer.com>
2018-10-13 19:48:45 +0200

    Modifiche Varie

--- src/main/java/com/xantrix/webapp/model/Utenti.java
+++ src/main/java/com/xantrix/webapp/model/Utenti.java
@@ -2,6 +2,9 @@ package com.xantrix.webapp.model;
 
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,7 +19,10 @@ public class Utenti
 	private String id;
 	
 	@Indexed(unique = true)
+	@Size(min = 6, max = 80, message = "{Size.Utenti.userId.Validation}")
+	@NotNull(message = "{NotNull.Articoli.userId.Validation}")
 	private String userId;
+	@Size(min = 6, max = 80, message = "{Size.Utenti.password.Validation}")
 	private String password;
 	private String attivo;
 	
